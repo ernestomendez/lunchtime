8ce2ac22b92f3a742a9860d7df8c15004d46d0ec
Ernesto Mendez <nernesto@gmail.com>
2018-10-14 23:29:18 -0500

    empty springboot project

---
+++ src/main/java/mx/com/dxesoft/lunchtime/LunchtimeApplication.java
@@ -0,0 +1,12 @@
+package mx.com.dxesoft.lunchtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchtimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchtimeApplication.class, args);
+	}
+}
---
+++ src/test/java/mx/com/dxesoft/lunchtime/LunchtimeApplicationTests.java
@@ -0,0 +1,16 @@
+package mx.com.dxesoft.lunchtime;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LunchtimeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
