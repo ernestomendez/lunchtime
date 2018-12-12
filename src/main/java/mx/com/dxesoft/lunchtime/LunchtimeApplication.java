cd4ee72908bd19cd14bdd49532d94f3c26ecdf97
Ernesto Mendez <ernesto.mendez@ihg.com>
2018-12-12 13:01:05 -0600

    updated spring profiles

--- src/main/java/mx/com/dxesoft/lunchtime/LunchtimeApplication.java
+++ src/main/java/mx/com/dxesoft/lunchtime/LunchtimeApplication.java
@@ -2,11 +2,16 @@ package mx.com.dxesoft.lunchtime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class LunchtimeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LunchtimeApplication.class, args);
+		//SpringApplication.run(LunchtimeApplication.class, args);
+        new SpringApplicationBuilder()
+                .sources(LunchtimeApplication.class)
+                .profiles("dev")
+                .run(args);
 	}
 }
